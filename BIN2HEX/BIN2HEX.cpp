251e660b2fdab198b4df96d293bbbda0c82bd01f
CdTCzech <hrncirmirek@windowslive.com>
2016-08-23 16:15:07 +0200

    Basic encryption implementation and encryption example

--- BIN2HEX/BIN2HEX.cpp
+++ BIN2HEX/BIN2HEX.cpp
@@ -1,6 +1,8 @@
+#include <algorithm>
 #include <fstream>
 #include <iomanip>
 #include <iterator>
+#include <random>
 #include <vector>
 
 int main(int argc, char** argv)
@@ -10,49 +12,81 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	std::ifstream is(argv[1], std::ifstream::binary);
-	std::ofstream os("HEX.txt", std::ofstream::binary);
+	std::random_device randomDevice;
+	std::default_random_engine randomEngine(randomDevice());
+	std::uniform_int_distribution<int> randomDistribution(0x21, 0x7E);
 
-	if (is)
+	std::ifstream inputStream(argv[1], std::ifstream::binary);
+	std::ofstream outputStream("HEX.txt", std::ofstream::binary);
+
+	if (inputStream)
 	{
-		// Stop eating new lines in binary mode!!!
-		is.unsetf(std::ios::skipws);
+		// donn't skip newlines
+		inputStream.unsetf(std::ios::skipws);
 
-		// get its size:
-		std::streampos fileSize;
-
-		is.seekg(0, std::ios::end);
-		fileSize = is.tellg();
-		is.seekg(0, std::ios::beg);
+		// get file size
+		inputStream.seekg(0, std::ios::end);
+		auto fileSize = inputStream.tellg();
+		inputStream.seekg(0, std::ios::beg);
 
 		// reserve capacity
-		std::vector<unsigned char> vec;
-		vec.reserve(static_cast<size_t>(fileSize));
+		std::vector<unsigned char> originalBinary;
+		originalBinary.reserve(static_cast<size_t>(fileSize));
 
 		// read the data:
-		vec.insert(vec.begin(),
-			std::istream_iterator<unsigned char>(is),
+		originalBinary.insert(originalBinary.begin(),
+			std::istream_iterator<unsigned char>(inputStream),
 			std::istream_iterator<unsigned char>());
 
-
-		for (size_t i = 0; i < vec.size() - 1; ++i)
+		// strip 0x66 and 0x67 to new vector
+		std::vector<unsigned char> binary;
+		binary.reserve(static_cast<size_t>(fileSize));
+		for (const auto character : originalBinary)
 		{
-			if (vec[i] == 0x66 || vec[i] == 0x67)
+			if (character != 0x66 && character != 0x67)
 			{
-				continue;
-			}
-
-			os << "0x" << std::hex << std::setfill('0') << std::setw(2) << static_cast<unsigned int>(vec[i]) << ", ";
-				
-			if (vec[i] == 0xC3)
-			{
-				os << "\r\n";
+				binary.push_back(character);
 			}
 		}
-		os << "0x" << std::hex << std::setfill('0') << std::setw(2) << static_cast<unsigned int>(vec[vec.size() - 1]);
+
+		// initialize key vector
+		std::vector<unsigned char> key;
+		key.resize(binary.size());
+		std::generate(key.begin(), key.end(), [&]() { return randomDistribution(randomEngine); });
+
+		// print original binary in hex, key and encrypted binary in hex
+		outputStream << "ORIGINAL" << "\r\n";
+		for (size_t i = 0; i < binary.size() - 1; ++i)
+		{
+			outputStream << "0x" << std::hex << std::setfill('0') << std::setw(2) << static_cast<unsigned int>(binary[i]) << ", ";
+				
+			if (binary[i] == 0xC3)
+			{
+				outputStream << "\r\n";
+			}
+		}
+		outputStream << "0x" << std::hex << std::setfill('0') << std::setw(2) << static_cast<unsigned int>(binary[binary.size() - 1]);
+
+		outputStream << "\r\n" << "KEY" << "\r\n";
+		for (const auto keyChar : key)
+		{
+			outputStream << keyChar;
+		}
+
+		outputStream << "\r\n" << "ENCRYPTED" << "\r\n";
+		for (size_t i = 0; i < binary.size() - 1; ++i)
+		{
+			outputStream << "0x" << std::hex << std::setfill('0') << std::setw(2) << static_cast<unsigned int>(binary[i] ^ key[i]) << ", ";
+
+			if ((i + 1) % 20 == 0)
+			{
+				outputStream << "\r\n";
+			}
+		}
+		outputStream << "0x" << std::hex << std::setfill('0') << std::setw(2) << static_cast<unsigned int>(binary[binary.size() - 1] ^ key[key.size() - 1]);
 	}
 
-	os.close();
-	is.close();
+	outputStream.close();
+	inputStream.close();
 	return 0;
 }
